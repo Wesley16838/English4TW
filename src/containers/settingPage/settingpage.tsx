6f0b1c8b7a4e41a65b16e7f699e0da43ab5d3645
Wesley16838 <wesley16838@gmail.com>
2021-08-22 13:20:57 -0500

    Update settingpage.tsx

--- src/containers/settingPage/settingpage.tsx
+++ src/containers/settingPage/settingpage.tsx
@@ -6,7 +6,7 @@ import ProfileImage from "./../../components/profileimage/profileimage";
 import Button from "./../../components/button/button";
 import theme from "./../../utilities/theme.style";
 import images from "../../assets/images";
-
+import ModalContainer from "./../../components/modal/modal";
 import {
   StyleSheet,
   Text,
@@ -14,6 +14,8 @@ import {
   TouchableWithoutFeedback,
   Image,
   Switch,
+  Modal,
+  Alert,
 } from "react-native";
 import { DEVICE_WIDTH } from "../splashpage";
 const settingPage = ({
@@ -23,158 +25,173 @@ const settingPage = ({
   navigation: any;
   route: any;
 }) => {
-  const handleOnClick = () => { };
+  const handleOnClick = () => {};
   const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [modalVisible, setModalVisible] = useState(false);
   const isLoggedIn: any = useSelector(
     (state: any) => state.user.isLoggedIn,
     shallowEqual
   );
   return (
-    <LinearGradient
-      colors={[theme.BACKGROUND_COLOR_1, theme.BACKGROUND_COLOR_2]}
-      style={styles.container}
-    >
-      <SafeAreaView
-        style={{
-          height: "100%",
-          alignItems: "center",
-          width: DEVICE_WIDTH,
+    <>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
         }}
       >
-        <ProfileImage
-          name={images.icons.default_profileimage}
-          size={130}
-          customStyle={{
-            height: 130,
-            width: 130,
-            marginTop: 25,
-            marginBottom: 20,
-            borderRadius: 65,
-          }}
+        <ModalContainer
+          content={["慢", "中", "快"]}
+          title={"播放速度"}
+          onCancel={() => setModalVisible(false)}
         />
-        <Button
-          title="登入"
-          onPress={() => navigation.push('loginPage')}
-          customStyle={{
-            width: 150,
-            height: 36,
-            borderRadius: 20,
-            flexDirection: "row",
-            marginBottom: 44,
+      </Modal>
+      <LinearGradient
+        colors={[theme.BACKGROUND_COLOR_1, theme.BACKGROUND_COLOR_2]}
+        style={styles.container}
+      >
+        <SafeAreaView
+          style={{
+            height: "100%",
+            alignItems: "center",
+            width: DEVICE_WIDTH,
           }}
-          imageSize={{
-            width: 16,
-            height: 16,
-            marginRight: 7,
-          }}
-          type="1"
-          image={""}
-        />
-        <View>
-          {true && <TouchableWithoutFeedback
-            accessible={true}
-            accessibilityLabel={"個人資訊"}
-            accessibilityHint={"個人資訊"}
-            onPress={() => navigation.push('personalprofilePage')}
-          >
-            <View style={styles.sectionRow}>
-              <Text style={styles.sectionText}>{"個人資訊"}</Text>
-              <Image
-                style={styles.sectionArrow}
-                source={images.icons.rightarrow_icon}
-              />
-            </View>
-          </TouchableWithoutFeedback>}
-          <TouchableWithoutFeedback
-            accessible={true}
-            accessibilityLabel={"特色介紹"}
-            accessibilityHint={"特色介紹"}
-            onPress={() => console.log('coming soon')}
-          >
-            <View style={styles.sectionRow}>
-              <Text style={styles.sectionText}>{"特色介紹"}</Text>
-              <Image
-                style={styles.sectionArrow}
-                source={images.icons.rightarrow_icon}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback
-            accessible={true}
-            accessibilityLabel={"意見回饋"}
-            accessibilityHint={"意見回饋"}
-            onPress={() => navigation.push('reviewPage')}
-          >
-            <View style={styles.sectionRow}>
-              <Text style={styles.sectionText}>{"意見回饋"}</Text>
-              <Image
-                style={styles.sectionArrow}
-                source={images.icons.rightarrow_icon}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback
-            accessible={true}
-            accessibilityLabel={"開發版號"}
-            accessibilityHint={"開發版號"}
-            onPressIn={() => { }}
-            onPressOut={() => { }}
-          >
-            <View
-              style={[
-                styles.sectionRow
-              ]}
+        >
+          <ProfileImage
+            name={images.icons.default_profileimage}
+            size={130}
+            customStyle={{
+              height: 130,
+              width: 130,
+              marginTop: 25,
+              marginBottom: 20,
+              borderRadius: 65,
+            }}
+          />
+          <Button
+            title="登入"
+            onPress={() => navigation.push("loginPage")}
+            customStyle={{
+              width: 150,
+              height: 36,
+              borderRadius: 20,
+              flexDirection: "row",
+              marginBottom: 44,
+            }}
+            imageSize={{
+              width: 16,
+              height: 16,
+              marginRight: 7,
+            }}
+            type="1"
+            image={""}
+          />
+          <View>
+            {true && (
+              <TouchableWithoutFeedback
+                accessible={true}
+                accessibilityLabel={"個人資訊"}
+                accessibilityHint={"個人資訊"}
+                onPress={() => navigation.push("personalprofilePage")}
+              >
+                <View style={styles.sectionRow}>
+                  <Text style={styles.sectionText}>{"個人資訊"}</Text>
+                  <Image
+                    style={styles.sectionArrow}
+                    source={images.icons.rightarrow_icon}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            )}
+            <TouchableWithoutFeedback
+              accessible={true}
+              accessibilityLabel={"特色介紹"}
+              accessibilityHint={"特色介紹"}
+              onPress={() => console.log("coming soon")}
             >
-              <Text style={styles.sectionText}>{"開發版號"}</Text>
-              <Text style={styles.version}>{"0.2.1"}</Text>
-            </View>
-          </TouchableWithoutFeedback>
-          {true && <TouchableWithoutFeedback
-            accessible={true}
-            accessibilityLabel={"離線模式"}
-            accessibilityHint={"離線模式"}
-            onPressIn={() => { }}
-            onPressOut={() => { }}
-          >
-            <View
-              style={[
-                styles.sectionRow
-              ]}
+              <View style={styles.sectionRow}>
+                <Text style={styles.sectionText}>{"特色介紹"}</Text>
+                <Image
+                  style={styles.sectionArrow}
+                  source={images.icons.rightarrow_icon}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              accessible={true}
+              accessibilityLabel={"意見回饋"}
+              accessibilityHint={"意見回饋"}
+              onPress={() => navigation.push("reviewPage")}
             >
-              <Text style={styles.sectionText}>{"離線模式"}</Text>
-              <Switch
-                trackColor={{ false: 'rgba(120, 120, 128, 0.16)', true: theme.PRIMARY_COLOR_DEFAULT }}
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-          }
-          {true && <TouchableWithoutFeedback
-            accessible={true}
-            accessibilityLabel={"播放速度"}
-            accessibilityHint={"播放速度"}
-            onPressIn={() => { }}
-            onPressOut={() => { }}
-          >
-            <View
-              style={[
-                styles.sectionRow,
-                { borderBottomColor: "#96CACA", borderBottomWidth: 1 },
-              ]}
+              <View style={styles.sectionRow}>
+                <Text style={styles.sectionText}>{"意見回饋"}</Text>
+                <Image
+                  style={styles.sectionArrow}
+                  source={images.icons.rightarrow_icon}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              accessible={true}
+              accessibilityLabel={"開發版號"}
+              accessibilityHint={"開發版號"}
+              onPressIn={() => {}}
+              onPressOut={() => {}}
             >
-              <Text style={styles.sectionText}>{"播放速度"}</Text>
-              <Image
-                style={styles.sectionArrow}
-                source={images.icons.rightarrow_icon}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-          }
-        </View>
-      </SafeAreaView>
-    </LinearGradient>
+              <View style={[styles.sectionRow]}>
+                <Text style={styles.sectionText}>{"開發版號"}</Text>
+                <Text style={styles.version}>{"0.2.1"}</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            {true && (
+              <TouchableWithoutFeedback
+                accessible={true}
+                accessibilityLabel={"離線模式"}
+                accessibilityHint={"離線模式"}
+                onPressIn={() => {}}
+                onPressOut={() => {}}
+              >
+                <View style={[styles.sectionRow]}>
+                  <Text style={styles.sectionText}>{"離線模式"}</Text>
+                  <Switch
+                    trackColor={{
+                      false: "rgba(120, 120, 128, 0.16)",
+                      true: theme.PRIMARY_COLOR_DEFAULT,
+                    }}
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            )}
+            {true && (
+              <TouchableWithoutFeedback
+                accessible={true}
+                accessibilityLabel={"播放速度"}
+                accessibilityHint={"播放速度"}
+                onPress={() => setModalVisible(true)}
+              >
+                <View
+                  style={[
+                    styles.sectionRow,
+                    { borderBottomColor: "#96CACA", borderBottomWidth: 1 },
+                  ]}
+                >
+                  <Text style={styles.sectionText}>{"播放速度"}</Text>
+                  <Image
+                    style={styles.sectionArrow}
+                    source={images.icons.rightarrow_icon}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            )}
+          </View>
+        </SafeAreaView>
+      </LinearGradient>
+    </>
   );
 };
 const styles = StyleSheet.create({
@@ -193,7 +210,7 @@ const styles = StyleSheet.create({
     width: DEVICE_WIDTH,
     borderTopColor: "#96CACA",
     borderTopWidth: 1,
-    backgroundColor: theme.COLOR_WHITE
+    backgroundColor: theme.COLOR_WHITE,
   },
   sectionText: {},
   version: {
